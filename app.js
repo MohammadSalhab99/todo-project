b760769dc2920b719c561fcf2c7843b68e509a72
MohammadSalhab99 <mohammadsalhab8@gmail.com>
2022-01-23 22:53:01 +0200

    add the changes

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var getName = prompt("What is your name? ");
 
 var gender = prompt("what is your gender");
-if (gender != "male" && gender != "female") {
+if (gender != "male" && gender != "female")
     alert("invalid input")
-
-}
-
+else if (gender == "male")
+    alert("welcome Mr." + getName)
+else
+    alert("welocme Ms." + getName)
 
 var age = prompt("what is your age");
 if (age <= 0)
     alert("age is invalid");
-var bool = confirm("Do you want to skip welcoming??");
+var bool = confirm("Do you want to skip the welcoming message ??");
 if (bool != true)
-    alert("welcome to my todo list ");
+    alert("Welcome to My todo list website, this website will help you to memorize your tasks and do them in the best way");
